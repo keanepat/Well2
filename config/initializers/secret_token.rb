50dea892a31e8f20c1f981b62755cc5824cb1fb2
Patrick Keane <keanepat@Patricks-MacBook-Air.local>
2014-08-13 13:42:27 -0400

    getting around to completing the Well application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Well2::Application.config.secret_key_base = '1fe0961a1ecfd0b37a35dbd2bc4135ece666b9a47373e6575d271376cac6f745f08b19bf1720d5026c516b7ed94ad98d3b07da0e2d15c4ef34823ea06268bac0'
